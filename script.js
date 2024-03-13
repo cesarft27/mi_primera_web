ea7cec477d16da435cf73da92ab6246ff47a0cfe
Cesar Forero <cesar.forerot@gmail.com>
2024-03-12 19:39:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let titulo = document.querySelector("h1");
+console.log(titulo);
+
+/* 
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+
+if(textoTitulo == nombre){
+    titulo.innerHTML = 'XXXXX';
+} 
+else {console.log("No cambia");};
+ */
